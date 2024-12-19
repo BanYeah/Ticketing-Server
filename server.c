ac23602154137eae89b7666eebad35d39fb7bfb7
BanYeah <04smailing@gmail.com>
2024-12-20 07:46:42 +0900

    Feat: Server Library

---
+++ server.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
