7c2218f39dabfe8eb2f7291a37f61ea1d89e084f
BanYeah <04smailing@gmail.com>
2024-12-20 08:08:03 +0900

    Style: Indent Missing

--- client.c
+++ client.c
@@ -16,43 +16,42 @@ typedef struct _query {
 } query;
 
 int main (int argc, char *argv[]) {
-    int client_socket = socket(PF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_addr;
-
-    if (argc < 3) {
-	printf("argv[1]: server address, argv[2]: port number\n");
-	exit(1);
+	if (argc < 3) { // help message
+		printf("argv[1]: server address, argv[2]: port number\n");
+		exit(1);
     }
 
+    int connection_socket = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(atoi(argv[2]));
     server_addr.sin_addr.s_addr = inet_addr(argv[1]);
 
-    if (connect(client_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
-	printf("Connection failed\n");
-	exit(1);
+    if (connect(connection_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
+		printf("Connection failed\n");
+		exit(1);
     }
-    //printf("Connected to server! Now querying...\n");
+    // printf("Connected to server! Now querying...\n");
 
     while (1) {
-	query q;
-	scanf("%d %d %d", &q.user, &q.action, &q.seat);
-	printf("%d %d %d\n", q.user, q.action, q.seat);
-	send(client_socket, &q, sizeof(q), 0);
-	int ret;
-	if (!(q.user | q.action | q.seat))
-	{
-	    int arr[256];
-	    recv(client_socket, arr, sizeof(arr), 0);
-	    int i;
-	    for(i=0;i<=255;i++) printf("%d ", arr[i]);
-	    printf("\n");
-	    break;
-	}
-	else{
-	    recv(client_socket, &ret, sizeof(ret), 0);
-	    printf("%d\n", ret);
-	}
+		query q;
+		scanf("%d %d %d", &q.user, &q.action, &q.seat);
+		printf("%d %d %d\n", q.user, q.action, q.seat);
+		send(connection_socket, &q, sizeof(q), 0);
+
+		if (!(q.user | q.action | q.seat)) { // After send (0, 0, 0)
+			int arr[256];
+			recv(connection_socket, arr, sizeof(arr), 0);
+
+			for(int i = 0; i <= 255; i++) printf("%d ", arr[i]);
+			printf("\n");
+			break;
+		}
+		else {
+			int ret
+			recv(connection_socket, &ret, sizeof(ret), 0);
+			printf("%d\n", ret);
+		}
     }
 
 }
